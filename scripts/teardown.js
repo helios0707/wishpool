cfd9281236e672f4f688942e6faa4c52fafa24d9
helios0707 <helios19880707@yahoo.com>
2015-07-21 13:01:59 +0300

    scripts

---
+++ scripts/teardown.js
@@ -0,0 +1,11 @@
+'use strict';
+var db = require("org/arangodb").db;
+
+function dropCollection(name) {
+  var collectionName = applicationContext.collectionName(name);
+  db._drop(collectionName);
+}
+
+dropCollection("activities");
+dropCollection("users");
+dropCollection("groups");
